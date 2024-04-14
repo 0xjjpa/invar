fcdf34b9ccd839f35080a6c8876ba4bce4dee113
jjperezaguinaga <me@jjperezaguinaga.com>
2024-04-14 17:54:21 +0000

    feature: Simplified PDF loading to url isntead of render

--- app/src/components/INVAR/INVARPDF.tsx
+++ app/src/components/INVAR/INVARPDF.tsx
@@ -1,26 +1,13 @@
-import { Document, Page, pdfjs } from "react-pdf";
 import { usePDFStore } from "./INVARStore";
-import { useEffect } from "react";
-import { Box } from "@chakra-ui/react";
+import { Link as ChakraLink, Text } from "@chakra-ui/react";
 
 export const INVARPDF = () => {
   const documentURL = usePDFStore((state) => state.documentURL);
 
-  useEffect(() => {
-    pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-      "pdfjs-dist/build/pdf.worker.min.js",
-      import.meta.url
-    ).toString();
-  }, []);
-
   return (
     <>
       {documentURL ? (
-        <Box w="100px">
-          <Document file={documentURL}>
-            <Page pageNumber={1} />
-          </Document>
-        </Box>
+        <Text>PDF loaded, find it <ChakraLink isExternal href={documentURL}>here</ChakraLink></Text>
       ) : (
         "Start demo to load sample file."
       )}
