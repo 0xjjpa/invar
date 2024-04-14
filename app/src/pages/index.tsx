a87c94fe6a84a80535a0fadab2fa21c2d63d6f17
jjperezaguinaga <me@jjperezaguinaga.com>
2024-04-14 16:54:01 +0000

    init: Initial commit

---
+++ app/src/pages/index.tsx
@@ -0,0 +1,48 @@
+import {
+  Link as ChakraLink,
+  Text,
+  Code,
+  List,
+  ListIcon,
+  ListItem,
+} from "@chakra-ui/react";
+import { CheckCircleIcon, LinkIcon } from "@chakra-ui/icons";
+
+import { Hero } from "../components/Hero";
+import { Container } from "../components/Container";
+import { Main } from "../components/Main";
+import { DarkModeSwitch } from "../components/DarkModeSwitch";
+import { CTA } from "../components/CTA";
+import { Footer } from "../components/Footer";
+
+const Index = () => (
+  <Container height="100vh">
+    <Hero />
+    <Main>
+      <Text color="text" fontSize={'sm'} textAlign={'center'}>
+      A decentralized system for secure, tamper-proof verification of signed documents,
+      ensuring <Code>(I)</Code>ntegrity, <Code>(N)</Code>on-<Code>(R)</Code>epudation, <Code>(V)</Code>alidity and <Code>(A)</Code>uthenticity through digital signatures, <ChakraLink href="https://sign.global/" isExternal textDecoration={'Underline'}>Sign Protocol</ChakraLink>, and blockchain.
+      </Text>
+
+      <List spacing={3} my={0} color="text">
+        <ListItem>
+          👤 <Code>Who?</Code>Professionals in any field who need to issue secure, official or legally binding documents.
+        </ListItem>
+        <ListItem>
+          ❌ <Code>Why?</Code>Document forgery, jeopardizing the reliability and trust in critical documents.
+        </ListItem>
+        <ListItem>
+          ⚙️ <Code>How?</Code>By issuing PKI-enabled, traceable and permanent attestations through decentralized tech.
+        </ListItem>
+      </List>
+    </Main>
+
+    <DarkModeSwitch />
+    <Footer>
+      <Text>Made with 🔒 by <ChakraLink href="https://x.com/0xjjpa" isExternal>0xjjpa</ChakraLink></Text>
+    </Footer>
+    <CTA />
+  </Container>
+);
+
+export default Index;
